44d262d490b3a12418ebba9c23fb0ff3eae8e164
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-07 14:47:38 -0500

    Update searchLib.js

--- search/searchLib.js
+++ search/searchLib.js
@@ -1,1 +1,11 @@
-
+function searchtext(arr,txt) {
+var d = txt.split(" ");
+var a = 0;
+  for(var i=0; i<d.length; i++) {
+  if (arr.search(d[i]!==-1) {
+      a++
+      
+      }
+  }
+      return a;
+}
