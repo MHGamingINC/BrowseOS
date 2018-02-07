6392719cdf98b40fe22d7beeb0ade38ff9855c3d
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-07 14:48:45 -0500

    Update searchLib.js

--- search/searchLib.js
+++ search/searchLib.js
@@ -2,7 +2,7 @@ function searchtext(arr,txt) {
 var d = txt.split(" ");
 var a = 0;
   for(var i=0; i<d.length; i++) {
-  if (arr.search(d[i]!==-1) {
+  if (arr.search(d[i])!==-1) {
       a++
       
       }
